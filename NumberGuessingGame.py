4b309bbbdaac5c6713820fead2fc94e1f233eaab
nicolebogart <nicolemariebogart@gmail.com>
2022-08-12 11:22:15 -0700

    Saving as is currently. Allowing for one too many guesses, and treating Y or N response to play again as a guess in a new game.

--- NumberGuessingGame.py
+++ NumberGuessingGame.py
@@ -8,33 +8,37 @@ print('Ready? Guess the number!')
 
 my_number = random.randint(1, 100)
 guess_count = 0
-answer = input()
+user_guess = int(input())
+# answer = input()
 
-while answer != 'N':
-    while 0 <= guess_count <= 10:
+# while answer != 'N':
+while 0 <= guess_count <= 10:
+    if guess_count == 10:
+        print("You've run out of guesses and you didn't guess my number! Try again?!")
+        print("Please enter Y or N!")
+        answer = input().upper()
+        if answer == 'Y':
+            guess_count = 0
+        else:
+            print("Goodbye!")
+    elif user_guess > my_number:
+        print("Too high! Try again!")
+        guess_count += 1
+        print("You have ", 10 - guess_count, " guesses remaining!")
         user_guess = int(input())
-        if user_guess > my_number:
-            print("Too high! Try again!")
-            guess_count += 1
-            print("You have ", 10 - guess_count, " guesses remaining!")
-        elif user_guess < my_number:
-            print("Too low! Try again!")
-            guess_count += 1
-            print("You have ", 10 - guess_count, " guesses remaining!")
-        elif user_guess == my_number:
-            print(f"You got it in {guess_count} guesses! My number was {my_number}!")
-            print("Would you like to play again? Please enter Y or N!")
-            answer = input().upper()
-            if answer == 'Y':
-                guess_count = 0
-            else:
-                print("Goodbye!")
-        elif guess_count == 10:
-            print("You've run out of guesses and you didn't guess my number! Try again?!")
-            print("Please enter Y or N!")
-            answer = input().upper()
-            if answer == 'Y':
-                guess_count = 0
-            else:
-                print("Goodbye!")
+    elif user_guess < my_number:
+        print("Too low! Try again!")
+        guess_count += 1
+        print("You have ", 10 - guess_count, " guesses remaining!")
+        user_guess = int(input())
+    elif user_guess == my_number:
+        print(f"You got it in {guess_count} guesses! My number was {my_number}!")
+        print("Would you like to play again? Please enter Y or N!")
+        answer = input().upper()
+        if answer == 'Y':
+            guess_count = 0
+
+        else:
+            print("Goodbye!")
+
 
